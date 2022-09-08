776a13180d1ab564c96476faafb523b1ea16c9fc
igor <zhurkoigor@gmail.com>
2022-09-08 22:17:09 +0300

    Prototype

---
+++ src/prototype/HouseFactory.java
@@ -0,0 +1,18 @@
+package prototype;
+
+public class HouseFactory {
+
+    House house;
+
+    public HouseFactory(House house) {
+        this.house = house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    House cloneHouse() {
+        return (House) house.clone();
+    }
+}
---
+++ src/prototype/Location.java
@@ -0,0 +1,5 @@
+package prototype;
+
+public enum Location {
+    CITY, COUNTRY
+}
